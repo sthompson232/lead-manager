4b4abde58d4a7063f2c7fb1cf76a23b126add9ac
Sam Thompson <samthompson292@gmail.com>
2021-04-11 17:39:33 +0100

    login functionality

--- leadmanager/frontend/src/reducers/auth.js
+++ leadmanager/frontend/src/reducers/auth.js
@@ -1,7 +1,9 @@
 import {
     USER_LOADED,
     USER_LOADING,
-    AUTH_ERROR
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
 } from '../actions/types';
 
 const initialState = {
@@ -24,8 +26,22 @@ export default function(state = initialState, action) {
                 isAuthenticated: true,
                 isLoading: false,
                 user: action.payload
-            }
+            };
+        case LOGIN_SUCCESS:
+            localStorage.setItem('token', action.payload.token);
+            return {
+                ...state,
+                ...action.payload,
+                isAuthenticated: true,
+                isLoading: false
+            };
+        // case LOGOUT_SUCCESS:
+        //     return {
+        //         ...state,
+        //         leads: [ ]
+        //     };
         case AUTH_ERROR:
+        case LOGIN_FAIL:
             localStorage.removeItem('token');
             return {
                 ...state,
